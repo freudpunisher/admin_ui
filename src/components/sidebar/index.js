e72487d6852121ceba6ccb020edbfd969339b4f1
freudpunisher <punishergte@gmail.com>
2024-09-10 05:14:19 +0200

    almost done

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -78,6 +78,72 @@ const Sidebar = () => {
             </span>
           </Button>
         </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <MdDashboard />
+            </span>
+            Dashboard
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <MdProductionQuantityLimits />
+            </span>
+            Products
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoCartOutline />
+            </span>
+            Orders
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoMdMail />
+            </span>
+            Messages
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoIosNotifications />
+            </span>
+            notifications
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoIosSettings />
+            </span>
+            Settings
+            <span className="arrow">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
       </ul>
     </div>
   );
